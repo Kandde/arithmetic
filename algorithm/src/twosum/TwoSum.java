29569e3dddde6490d0398bd2cf7e793ac80ee803
zhaohongxin <zhaohongxin@wejoydata.com>
2020-05-31 20:06:43 +0800

    tow sum

---
+++ algorithm/src/twosum/TwoSum.java
@@ -0,0 +1,26 @@
+package twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhaohongxin
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(32);
+        for(int i = 0; i < nums.length; i ++) {
+            map.put(nums[i], i);
+        }
+        System.out.println(map.containsKey(7));
+        for (int i = 0; i < nums.length; i ++) {
+            int tmpTarget = target - nums[i];
+            if (map.containsKey(tmpTarget) && map.get(tmpTarget) != i) {
+                return new int[] {i, map.get(tmpTarget)};
+            }
+        }
+        return null;
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+LeetCode
+=====
+
+### LeetCode Algorithm
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+|1|[Two Sum](https://leetcode.com/problems/two-sum/)| [Java](./algorithms/cpp/twoSum/twoSum.cpp)|Easy|
+
+
